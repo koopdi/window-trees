098c34a0378f20de710af217629e9ea9af1db35d
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-05-19 12:39:24 -0700

    doxygen comments for workspace manager header

--- src/WindowManager/WM.h
+++ src/WindowManager/WM.h
@@ -1,10 +1,40 @@
+/***********************************************************
+ * Class Workspace Manager
+ * The workspace manager keeps track of the workspaces.
+ * @file WM.h
+ **********************************************************/
+
+#include "ServerInterface.h"
 #include "WMWorkspace.h"
 #include "xtypes.h"
-#include "ServerInterface.h"
 
-class WM {
-    vector<WMWorkspace> workspaces;
-    void update(XEvent& ev);
+#include <set>
+
+/**
+ * @brief Manage Workspaces
+ *
+ */
+class WM
+{
 public:
-    WM(ServerInterface* server);
+	// public methods ----------------------------------------
+	/**
+	 * @brief Construct a new WM object
+	 * @param server
+	 */
+	WM(ServerInterface* server);
+
+	/**
+	 * @brief do something with events?
+	 * @param ev
+	 */
+	void update(XEvent& ev);
+
+private:
+	// private attributes ------------------------------------
+
+	/// A collection of workspaces.
+	std::set<WMWorkspace> workspaces;
+
+	// private methods ---------------------------------------
 };
