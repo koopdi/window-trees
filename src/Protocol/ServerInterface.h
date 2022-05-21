ae7ec374ddb364db966ac89f4b18d2910b0c97d2
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-05-21 09:03:15 -0700

    formatting

--- src/Protocol/ServerInterface.h
+++ src/Protocol/ServerInterface.h
@@ -3,8 +3,8 @@
 
 #include "types.h"
 #include "xtypes.h"
-#include <vector>
 #include <functional>
+#include <vector>
 
 /**
  * @brief An abstract class that provides an interface for window
@@ -16,24 +16,25 @@ class ServerInterface;
 /// The display server calls this function, when the window manager starts.
 typedef std::function<void(ServerInterface* server)> InitHandlerFn;
 /// The server calls this function when events are produced.
-typedef std::function<void(ServerInterface* server, XEvent* event)> EventHandlerFn;
+typedef std::function<void(ServerInterface* server, XEvent* event)>
+    EventHandlerFn;
 
 class ServerInterface
 {
 public:
-	virtual void setArea(long windowID, Area area) 		= 0;
-	virtual Area getArea(long windowID)					= 0;
-	virtual Area getScreenSize(long screenID)			= 0;
-	virtual std::vector<long> getScreens()				= 0;
-	virtual std::vector<long> getWindows(long screenID)	= 0;
+	virtual void setArea(long windowID, Area area)      = 0;
+	virtual Area getArea(long windowID)                 = 0;
+	virtual Area getScreenSize(long screenID)           = 0;
+	virtual std::vector<long> getScreens()              = 0;
+	virtual std::vector<long> getWindows(long screenID) = 0;
 
-/**
- * @brief Set the Init Callback object
- * Provides a concrete server interface.
- * @param fn
- */
-	virtual void setInitCallback(InitHandlerFn fn)		= 0;
-	virtual void setEventCallback(EventHandlerFn fn)	= 0;
-	virtual void run()									= 0;
+	/**
+	 * @brief Set the Init Callback object
+	 * Provides a concrete server interface.
+	 * @param fn
+	 */
+	virtual void setInitCallback(InitHandlerFn fn)      = 0;
+	virtual void setEventCallback(EventHandlerFn fn)    = 0;
+	virtual void run()                                  = 0;
 };
 #endif
