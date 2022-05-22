a2b7d3af279c027825ea49a9e0a9a9e4ed480d40
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-05-21 18:54:30 -0700

    convert terminal cmd seqs from #define to char arr

--- src/Protocol/Logger.h
+++ src/Protocol/Logger.h
@@ -4,56 +4,56 @@
 
 #ifdef __linux__
 	//Regular text
-	#define BLK "\e[0;30m"
-	#define RED "\e[0;31m"
-	#define GRN "\e[0;32m"
-	#define YEL "\e[0;33m"
-	#define BLU "\e[0;34m"
-	#define MAG "\e[0;35m"
-	#define CYN "\e[0;36m"
-	#define WHT "\e[0;37m"
-	#define GRY "\e[1;30m"
+	const char BLK[] = "\e[0;30m";
+	const char RED[] = "\e[0;31m";
+	const char GRN[] = "\e[0;32m";
+	const char YEL[] = "\e[0;33m";
+	const char BLU[] = "\e[0;34m";
+	const char MAG[] = "\e[0;35m";
+	const char CYN[] = "\e[0;36m";
+	const char WHT[] = "\e[0;37m";
+	const char GRY[] = "\e[1;30m";
 	//Regular bold text
-	#define BBLK "\e[1;30m"
-	#define BRED "\e[1;31m"
-	#define BGRN "\e[1;32m"
-	#define BYEL "\e[1;33m"
-	#define BBLU "\e[1;34m"
-	#define BMAG "\e[1;35m"
-	#define BCYN "\e[1;36m"
-	#define BWHT "\e[1;37m"
+	const char BBLK[] = "\e[1;30m";
+	const char BRED[] = "\e[1;31m";
+	const char BGRN[] = "\e[1;32m";
+	const char BYEL[] = "\e[1;33m";
+	const char BBLU[] = "\e[1;34m";
+	const char BMAG[] = "\e[1;35m";
+	const char BCYN[] = "\e[1;36m";
+	const char BWHT[] = "\e[1;37m";
 	//Reset
-	#define reset "\e[0m"
-	#define CRESET "\e[0m"
-	#define COLOR_RESET "\e[0m"
+	const char reset[] = "\e[0m";
+	const char CRESET[] = "\e[0m";
+	const char COLOR_RESET[] = "\e[0m";
 
-	#define MSG_LOGGING_BEGIN "BEGIN LOGGING:"
+	const char MSG_LOGGING_BEGIN[] = "BEGIN LOGGING:";
 #elif _WIN32
 //Regular text
-	#define BLK ""
-	#define RED ""
-	#define GRN ""
-	#define YEL ""
-	#define BLU ""
-	#define MAG ""
-	#define CYN ""
-	#define WHT ""
-	#define GRY ""
+	const char BLK[] = "";
+	const char RED[] = "";
+	const char GRN[] = "";
+	const char YEL[] = "";
+	const char BLU[] = "";
+	const char MAG[] = "";
+	const char CYN[] = "";
+	const char WHT[] = "";
+	const char GRY[] = "";
 	//Regular bold text
-	#define BBLK ""
-	#define BRED ""
-	#define BGRN ""
-	#define BYEL ""
-	#define BBLU ""
-	#define BMAG ""
-	#define BCYN ""
-	#define BWHT ""
+	const char BBLK[] = "";
+	const char BRED[] = "";
+	const char BGRN[] = "";
+	const char BYEL[] = "";
+	const char BBLU[] = "";
+	const char BMAG[] = "";
+	const char BCYN[] = "";
+	const char BWHT[] = "";
 	//Reset
-	#define reset ""
-	#define CRESET ""
-	#define COLOR_RESET ""
+	const char reset[] = "";
+	const char CRESET[] = "";
+	const char COLOR_RESET[] = "";
 
-	#define MSG_LOGGING_BEGIN "BEGIN LOGGING: [Notice]: Colored output is not supported on your platform."
+	const char MSG_LOGGING_BEGIN "BEGIN LOGGING: [Notice]: Colored output is not supported on your platform.";
 #endif
 
 enum LogLevel {
