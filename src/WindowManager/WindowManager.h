ff1f42a5d244fd6ca6013c331222bf054dea4bb7
Andrew Hanson <ahanso2@uw.edu>
2022-05-23 14:54:59 -0700

    added comments and flesh to WindowManager directory

--- src/WindowManager/WindowManager.h
+++ src/WindowManager/WindowManager.h
@@ -1,21 +1,26 @@
+/**
+ * @file WindowManager.h
+ * @author Andrew Hanson, Christen Spadevechia, Travis McKinney (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-23
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #pragma once
 
-/***********************************************************
- * Class Window Manager
- * The window manager keeps track of the workspaces.
- * @file WindowManager.h
- **********************************************************/
+#include <set>
 
 #include "ServerInterface.h"
 #include "Workspace.h"
 #include "xtypes.h"
 
-#include <set>
-
-/**
- * @brief Manage Workspaces
- *
- */
+/***********************************************************
+ * Class Window Manager
+ * The window manager keeps track of the workspaces.
+ **********************************************************/
 class WindowManager
 {
 public:
--- src/WindowManager/WindowTree.h
+++ src/WindowManager/WindowTree.h
@@ -1,28 +1,137 @@
+/**
+ * @file WindowTree.h
+ * @author Andrew Hanson, Christen Spadevechia, Travis McKinney (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-23
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #pragma once
 
 #include <functional>
 
 #include "WindowNode.h"
 
+/**
+ * @brief
+ *
+ */
 class WindowTree {
 public:
-    WindowTree();
-    bool add(bool partVertically, int windowID, double part1Size);
-    bool remove(int windowID);
-    WindowNode* get(int windowID);
+	// public methods ----------------------------------------
 
+	/**
+	 * @brief Construct a new Window Tree object
+	 *
+	 */
+    WindowTree();
+
+	/**
+	 * @brief
+	 *
+	 * @param windowID
+	 * @return WindowNode*
+	 */
+	WindowNode* get(int windowID);
+
+	/**
+	 * @brief
+	 *
+	 * @param partVertically
+	 * @param windowID
+	 * @param part1Size
+	 * @return true
+	 * @return false
+	 */
+    bool add(bool partVertically, int windowID, double part1Size);
+
+	/**
+	 * @brief
+	 *
+	 * @param windowID
+	 * @return true
+	 * @return false
+	 */
+    bool remove(int windowID);
+
+	/**
+	 * @brief
+	 *
+	 * @return true
+	 * @return false
+	 */
     bool isEmpty() const;
+
+	/**
+	 * @brief
+	 *
+	 * @param windowID
+	 * @return true
+	 * @return false
+	 */
     bool contains(int windowID) const;
+
+	/**
+	 * @brief Get the Size object
+	 *
+	 * @return int
+	 */
     int getSize() const;
+
+	/**
+	 * @brief Get the Num Windows object
+	 *
+	 * @return int
+	 */
     int getNumWindows() const;
 
 private:
-    void preOrderTraverse(WindowNode* node, const std::function<bool(WindowNode*)>& func) const;
-    void inOrderTraverse(WindowNode* node, std::function<bool(WindowNode*)> func) const;
-    void postOrderTraverse(WindowNode* node, std::function<bool(WindowNode*)> func) const;
-	void add(WindowNode*& node, bool partVertically, int windowID, double part1Size);
+	// private attributes ------------------------------------
 
+	// the number of windows
 	int numWindows;
+
+	// the size of the tree
     int size;
+
+	// a pointer to the root node of the tree
     WindowNode* root;
+
+	// private methods ----------------------------------------
+
+	/**
+	 * @brief
+	 *
+	 * @param node
+	 * @param func
+	 */
+    void preOrderTraverse(WindowNode* node, const std::function<bool(WindowNode*)>& func) const;
+
+	/**
+	 * @brief
+	 *
+	 * @param node
+	 * @param func
+	 */
+    void inOrderTraverse(WindowNode* node, std::function<bool(WindowNode*)> func) const;
+
+	/**
+	 * @brief
+	 *
+	 * @param node
+	 * @param func
+	 */
+    void postOrderTraverse(WindowNode* node, std::function<bool(WindowNode*)> func) const;
+
+	/**
+	 * @brief
+	 *
+	 * @param node
+	 * @param partVertically
+	 * @param windowID
+	 * @param part1Size
+	 */
+	void add(WindowNode*& node, bool partVertically, int windowID, double part1Size);
 };
