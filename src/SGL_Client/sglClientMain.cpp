ba303708bb6f784a22ce22c5830458cdab82e359
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-05-28 10:20:33 -0700

    key event handler close window when escape is pressed

--- src/SGL_Client/sglClientMain.cpp
+++ src/SGL_Client/sglClientMain.cpp
@@ -7,6 +7,14 @@ using namespace sgl;
 const int WINDOW_WIDTH  = 640;
 const int WINDOW_HEIGHT = 480;
 
+void keyEv(GEvent ev, GWindow& win)
+{
+	if(ev.getKeyCode()==GEvent::ESCAPE_KEY)
+	{
+		win.close();
+	}
+}
+
 int main()
 {
 	// create a window
@@ -18,9 +26,10 @@ int main()
 	win.setExitOnClose(true);
 	win.center();
 	win.drawImage("edmonds.png");
+	win.setWindowTitle("WindowTrees");
+	win.setKeyListener([window](GEvent ev) { keyEv(ev, *window); });
 
-	cout << "test" << endl;
-	delete window;
-	cout << "test2" << endl;
+	// this doesn't close the window
+	// delete window; // don't delete the window
 	return 0;
 }
