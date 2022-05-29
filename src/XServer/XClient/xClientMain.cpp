54a5e8571b7a96978a87fdc94bbca2412faab71a
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-05-29 15:49:28 -0700

    formatting, remove silenced include

--- src/XServer/XClient/xClientMain.cpp
+++ src/XServer/XClient/xClientMain.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
 #include "WindowManager.h"
-// #include "DisplayServer.h"
+#include <iostream>
 #include "ServerInterface.h"
 #include "XServer.h"
 
 #define _DEBUG_PRINT_
 
-int main(){
-	#ifdef _DEBUG_PRINT_
+int main()
+{
+#ifdef _DEBUG_PRINT_
 	std::cout << "new windowManager()" << std::endl;
-	#endif
+#endif
 
 	ServerInterface* server = new XServer();
 	WindowManager winMan(server);
 
-	#ifdef _DEBUG_PRINT_
+#ifdef _DEBUG_PRINT_
 	std::cout << "run" << std::endl;
-	#endif
+#endif
 
-	EventHandlerFn handlerFunc = [&winMan](XEvent* event) {
-		winMan.update(*event);
-	};
+	EventHandlerFn handlerFunc = [&winMan](XEvent* event)
+	{ winMan.update(*event); };
 
 	server->setEventCallback(handlerFunc);
 
 	server->run();
 
-	#ifdef _DEBUG_PRINT_
+#ifdef _DEBUG_PRINT_
 	std::cout << "return(0)" << std::endl;
-	#endif
+#endif
 }
