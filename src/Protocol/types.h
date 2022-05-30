41ccfcca63948bbf60c707efe0323615e85923bb
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-05-29 20:10:42 -0700

    Default initialize Area object to (-1)

--- src/Protocol/types.h
+++ src/Protocol/types.h
@@ -1,16 +1,24 @@
 #ifndef TYPES_H
 #define TYPES_H
-struct Point{
-    int x;
-    int y;
+struct Point
+{
+	int x;
+	int y;
 };
 
-struct Area{
+struct Area
+{
+	/**
+	 * @brief All values will default to -1.
+	 * @param x X position (of which corner?)
+	 * @param y Y position (of which corner?)
+	 * @param width Width of the rectangle.
+	 * @param height Height of the rectangle.
+	 */
+	Area(int x = -1, int y = -1, int width = -1, int height = -1);
 	int x;
 	int y;
 	int width;
 	int height;
 };
 #endif
-
-
