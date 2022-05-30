35de0b4a2af9ef3b6b43c5c3c559900d7580ccf2
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-05-29 20:42:30 -0700

    remove extraneous semicolons

--- src/SGLServer/SGLServer.cpp
+++ src/SGLServer/SGLServer.cpp
@@ -2,31 +2,34 @@
 
 SGLServer::SGLServer() {}
 
-void SGLServer::setArea(long windowID, Area area){};
+void SGLServer::setArea(long windowID, Area area){}
 
 Area SGLServer::getArea(long windowID)
 {
 	Area foo;
 	return foo;
-};
+}
 Area SGLServer::getScreenSize(long screenID)
 {
 	Area foo;
 	return foo;
-};
+}
 
 std::vector<long> SGLServer::getScreens()
 {
 	std::vector<long> foo;
 	return foo;
-};
+}
 
 std::vector<long> SGLServer::getWindows(long screenID)
 {
 	std::vector<long> foo;
 	return foo;
-};
+}
 
-void SGLServer::setInitCallback(InitHandlerFn fn){};
-void SGLServer::setEventCallback(EventHandlerFn fn){};
-void SGLServer::run(){};
+void SGLServer::setInitCallback(InitHandlerFn fn)
+{
+
+}
+void SGLServer::setEventCallback(EventHandlerFn fn){}
+void SGLServer::run(){}
