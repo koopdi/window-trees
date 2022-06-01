db38282c8cb1c1ba6e5707f65dcf6acb02fd46db
jd <johnnytriple33@gmail.com>
2022-05-31 17:10:41 -0700

    nitpick

--- src/SDLServer/ServerWindowSDL.cpp
+++ src/SDLServer/ServerWindowSDL.cpp
@@ -3,7 +3,7 @@
 ServerWindowSDL::ServerWindowSDL(long windowID, int depth, SDL_Rect rect, SDL_Color color) :
 	windowID(windowID), depth(depth), rect(rect), color(color){}
 
-void ServerWindowSDL::render(SDL_Renderer * ren) const{
+void ServerWindowSDL::render(SDL_Renderer* ren) const{
 	SDL_SetRenderDrawColor(ren, color.r, color.g, color.b, color.a);
 	SDL_RenderDrawRect(ren, &rect);
 }
