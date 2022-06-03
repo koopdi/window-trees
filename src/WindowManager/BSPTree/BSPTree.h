b07e1217f2815ade813e43b0f05afcdccbbf5577
jd <johnnytriple33@gmail.com>
2022-06-03 14:15:12 -0700

    Created WindowTreeInterface and BSPTree class Note: Has cmake issues

---
+++ src/WindowManager/BSPTree/BSPTree.h
@@ -0,0 +1,9 @@
+#include "ServerInterface.h"
+#include "WindowTreeInteraface.h"
+
+class BSPTree : public WindowTreeInterface
+{
+	virtual void render(ServerInterface* server) override;
+	virtual void addWindow(long windowID) override;
+	virtual void remWindow(long windowID) override;
+};
---
+++ src/WindowManager/WindowTreeInterface.h
@@ -0,0 +1,7 @@
+#include "ServerInterface.h"
+
+class WindowTreeInterface {
+	virtual void render(ServerInterface* server) = 0;
+	virtual void addWindow(long windowID)        = 0;
+	virtual void remWindow(long windowID)        = 0;
+};
