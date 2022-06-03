58bb0343f0ce90c50e5e700e66f0bf72be7bbb08
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-06-03 10:04:39 -0700

    lemon fir layout specification

--- src/WindowManager/LemonFir/LemonFir.h
+++ src/WindowManager/LemonFir/LemonFir.h
@@ -23,3 +23,25 @@ private:
 	/// A pointer to the server.
 	ServerInterface* server;
 };
+
+/**
+ * @brief A non-citrous zest.
+ *
+	Each window occupies a leaf node, referred to as a WindowPane.
+
+	The parent of a WindowPane is a WindowNode and
+	 each of those WindowNodes may also have a parent.
+
+	WindowNodes bifurcate space either vertically or horizontally,
+	 depending on their internal state.
+
+	The panes are laid out by walking the tree and
+	 placing nodes either left/up or right/down.
+	 the available space is halved each time a pane is placed.
+
+	Automatic modes of split orientation include;
+	 Every Other - a child node splits opposite it's parent.
+	 Minimum Perimeter - the split angle is chosen to make the
+	  children as square as possible.
+ *
+ */
