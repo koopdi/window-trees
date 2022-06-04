317fde739f909cc1ef7cf767dceeb985b4c1e0b9
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-06-04 12:40:12 -0700

    Lemon Fir WM Update event handling

--- src/WindowManager/LemonFir/LemonFir.cpp
+++ src/WindowManager/LemonFir/LemonFir.cpp
@@ -1,24 +1,35 @@
 #include "LemonFir.h"
+#include <string>
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
 
-LemonFir::LemonFir(ServerInterface* server) : server(server)
+LemonFir::LemonFir(ServerInterface* server) : server(server) {}
+
+void LemonFir::update(ev::Event& ev)
 {
+	try {
+		cout << "Lemon Fir WM Update: ";
+		if (ev.type == ev::EventType::ADD) {
+			cout << "addWindow" << endl;
+			addWindow(ev.add.winID);
+		} else if (ev.type == ev::EventType::REMOVE) {
+			cout << "remWindow" << endl;
+			remWindow(ev.remove.winID);
+		}
+
+		render(server);
+
+	} catch (string error) {
+		cout << error << endl;
+	}
 }
 
-void LemonFir::update(ev::Event& ev){}
+void LemonFir::render(ServerInterface* server) {}
 
-void LemonFir::render(ServerInterface* server)
-{
-}
+void LemonFir::addWindow(long windowID) {}
 
-void LemonFir::addWindow(long windowID)
-{
-}
+void LemonFir::remWindow(long windowID) {}
 
-void LemonFir::remWindow(long windowID)
-{
-}
-
-void LemonFir::resize(Area area)
-{
-}
-
+void LemonFir::resize(Area area) {}
