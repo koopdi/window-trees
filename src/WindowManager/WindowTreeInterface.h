5e7301e52fea655408f9eca71f954ce114a63783
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-06-04 00:42:50 -0700

    interface comments

--- src/WindowManager/WindowTreeInterface.h
+++ src/WindowManager/WindowTreeInterface.h
@@ -1,11 +1,37 @@
 #pragma once
 
+/***********************************************************
+ * Window Tree Interface
+ *
+ * @brief This abstract class provides an interface for
+ *  different layout structures.
+ *
+ * @file WindowTreeInterface.h
+ * @author Andrew Hanson, Christen Spadevechia, Travis McKinney
+ */
+
 #include "ServerInterface.h"
-//        ServerInterface.h
 
 class WindowTreeInterface {
+	/**
+	 * @brief Update the position of windows in the tree.
+	 * @param server The tree calls methods of this server to
+	 * update the window positions and size.
+	 */
 	virtual void render(ServerInterface* server) = 0;
+	/**
+	 * @brief Tell the tree to manage this window's layout.
+	 * @param windowID The ID of the window.
+	 */
 	virtual void addWindow(long windowID)        = 0;
+	/**
+	 * @brief Tell the tree to stop managing this window's layout.
+	 * @param windowID The ID of the window.
+	 */
 	virtual void remWindow(long windowID)        = 0;
+	/**
+	 * @brief I have no idea.
+	 * @param area A space, a place, a time...
+	 */
 	virtual void resize(Area area);
 };
