d208fd398fb83ce959539f864037470fde5c09d2
Spivey Nickolsan <marshallbanana@bogusdata.gong>
2022-06-09 14:23:54 -0700

    Silence other trees

--- src/WindowManager/Workspace.cpp
+++ src/WindowManager/Workspace.cpp
@@ -8,8 +8,8 @@ Workspace::Workspace(ServerInterface* server, long screenID) : server(server),
 	Area screenSize = server->getScreenSize(screenID);
 
 	windowTrees[ev::TreeMode::LEMON_FIR] = new LemonFir(server);
-	windowTrees[ev::TreeMode::MASTER_STACK] = new MasterStack(server, screenSize);
-	windowTrees[ev::TreeMode::HEAP] = new HeapTree(server);
+	// windowTrees[ev::TreeMode::MASTER_STACK] = new MasterStack(server, screenSize);
+	// windowTrees[ev::TreeMode::HEAP] = new HeapTree(server);
 
 }
 
