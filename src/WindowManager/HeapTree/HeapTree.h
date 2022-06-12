b4ab25bcf3ceb2ec61384c13733925f9be6a1d28
Andrew Hanson <ahanso2@uw.edu>
2022-06-11 23:57:07 -0700

    more comments

--- src/WindowManager/HeapTree/HeapTree.h
+++ src/WindowManager/HeapTree/HeapTree.h
@@ -12,6 +12,8 @@
 #include "WindowNode.h"
 #include "types.h"
 
+// a class to represent one decktop/virtual screen which owns a set of trees
+// to hold and order windows
 class Workspace;
 
 /**
@@ -136,17 +138,53 @@ private:
 	int getNumWindows() const;
 
 	/**
-	 * @brief a function for debuging and isplaying the contents of the heap
+	 * @brief a function for debuging and displaying the contents of the heap
 	 *
 	 * @param heap a vector of windows
 	 */
 	void printHeap(const std::vector<HeapWindow*>& heap) const;
+
+	/**
+	 * @brief a function for debuging and displaying the contents of the tree
+	 *
+	 * @param root the root node
+	 */
 	void printSideways(WindowNode* root) const;
+
+	/**
+	 * @brief
+	 *
+	 * @param root a node of the tree
+	 * @param spaces a string to properly space the lines of the output
+	 */
 	void printSidewaysHelper(WindowNode* root, std::string spaces) const;
-	//void preOrderTraverse(WindowNode* node, forEachNode func) const;
+
+	// /**
+	//  * @brief a function that calls a closure on each node in a specific order
+	//  *
+	//  * @param node a node in the tree
+	//  * @param func a function to call on each node
+	//  */
+	// void preOrderTraverse(WindowNode* node, forEachNode func) const;
 	//void inOrderTraverse(WindowNode* node, forEachNode func) const;
 	//void postOrderTraverse(WindowNode* node, forEachNode func) const;
+
+	/**
+	 * @brief adds a node to the tree
+	 *
+	 * @param windowID a node ID to be added
+	 * @return true
+	 * @return false
+	 */
 	bool add(int windowID);
+
+	/**
+	 * @brief remove a node from the tree
+	 *
+	 * @param windowID a node ID to be removed
+	 * @return true
+	 * @return false
+	 */
 	bool remove(int windowID);
 
 	/// A function that is passed to a xyzTraverse function.
